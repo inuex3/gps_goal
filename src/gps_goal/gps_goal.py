93b90e909dd7810c49401d230748f02e70087948
Kosuke Inoue <mmts0106@gmail.com>
2019-06-29 01:11:14 +0900

    publish point

--- src/gps_goal/gps_goal.py
+++ src/gps_goal/gps_goal.py
@@ -6,9 +6,7 @@ import actionlib
 import tf
 
 from geographiclib.geodesic import Geodesic
-from actionlib_msgs.msg import GoalStatus
-from geometry_msgs.msg import PoseStamped
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import PoseStamped, PointStamped
 from sensor_msgs.msg import NavSatFix
 
 def DMS_to_decimal_format(lat,long):
@@ -63,11 +61,7 @@ def calc_goal(origin_lat, origin_long, goal_lat, goal_long):
 class GpsGoal():
   def __init__(self):
     rospy.init_node('gps_goal')
-
-    rospy.loginfo("Connecting to move_base...")
-    self.move_base = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-    self.move_base.wait_for_server()
-    rospy.loginfo("Connected.")
+    self.point_publisher = rospy.Publisher('/clicked_point', PointStamped, queue_size = 1)
 
     rospy.Subscriber('gps_goal_pose', PoseStamped, self.gps_goal_pose_callback)
     rospy.Subscriber('gps_goal_fix', NavSatFix, self.gps_goal_fix_callback)
@@ -78,7 +72,6 @@ class GpsGoal():
   def do_gps_goal(self, goal_lat, goal_long, z=0, yaw=0, roll=0, pitch=0):
     # Calculate goal x and y in the frame_id given the frame's origin GPS and a goal GPS location
     x, y = calc_goal(self.origin_lat, self.origin_long, goal_lat, goal_long)
-    # Create move_base goal
     self.publish_goal(x=x, y=y, z=z, yaw=yaw, roll=roll, pitch=pitch)
 
   def gps_goal_pose_callback(self, data):
@@ -96,33 +89,18 @@ class GpsGoal():
 
   def publish_goal(self, x=0, y=0, z=0, yaw=0, roll=0, pitch=0):
     # Create move_base goal
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = rospy.get_param('~frame_id','map')
-    goal.target_pose.pose.position.x = x
-    goal.target_pose.pose.position.y = y
-    goal.target_pose.pose.position.z =  z
-    quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
-    goal.target_pose.pose.orientation.x = quaternion[0]
-    goal.target_pose.pose.orientation.y = quaternion[1]
-    goal.target_pose.pose.orientation.z = quaternion[2]
-    goal.target_pose.pose.orientation.w = quaternion[3]
-    rospy.loginfo('Executing move_base goal to position (x,y) %s, %s, with %s degrees yaw.' %
-            (x, y, yaw))
-    rospy.loginfo("To cancel the goal: 'rostopic pub -1 /move_base/cancel actionlib_msgs/GoalID -- {}'")
-
-    # Send goal
-    self.move_base.send_goal(goal)
-    rospy.loginfo('Inital goal status: %s' % GoalStatus.to_string(self.move_base.get_state()))
-    status = self.move_base.get_goal_status_text()
-    if status:
-      rospy.loginfo(status)
-
-    # Wait for goal result
-    self.move_base.wait_for_result()
-    rospy.loginfo('Final goal status: %s' % GoalStatus.to_string(self.move_base.get_state()))
-    status = self.move_base.get_goal_status_text()
-    if status:
-      rospy.loginfo(status)
+    Point = PointStamped()
+    Point.header.stamp = rospy.Time.now()
+    Point.header.frame_id = rospy.get_param('~frame_id','map')
+    Point.point.x = x
+    Point.point.y = y
+    Point.point.z = z
+    #quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+    #goal.target_pose.pose.orientation.x = quaternion[0]
+    #goal.target_pose.pose.orientation.y = quaternion[1]
+    #goal.target_pose.pose.orientation.z = quaternion[2]
+    #goal.target_pose.pose.orientation.w = quaternion[3]
+    self.point_publisher.publish(Point)        
 
 @click.command()
 @click.option('--lat', prompt='Latitude', help='Latitude')
